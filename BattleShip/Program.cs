746ba60b05d1e16a15fe3e255d88ce5a54c0e0d0
Neo Rosenlund <Neo.Rosenlund@elev.ga.ntig.se>
2024-03-21 10:59:39 +0100

    have an error, but have set up placing ship default

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -1,1 +1,63 @@
-﻿
+﻿//vi använder oss av array då brädet inte behöver ha en dynamisk storlek, och då blir en array bättre då den är lite snabbare när vi vill komma åt specifika element
+string[,] board1 = new string[8, 8];
+string[,] board2 = new string[8, 8];
+
+string[,] ShotBoard1 = new string[8, 8];
+string[,] ShotBoard2 = new string[8, 8];
+
+PlaceShip(board1);
+void Menu()
+{
+
+}
+void GameLoop()
+{
+
+}
+static string[,] PlaceShip(string[,] board)
+{
+Console.Clear();
+    for (int i = 0; i < board.GetLength(0); i++){
+        for(int j = 0; j < board.GetLength(1); j++){
+            if(board[i,j] == null){
+                Console.Write("O ");
+            }else{
+                Console.Write("X ");
+            }
+        }
+        Console.WriteLine();
+    }
+Console.WriteLine("Place your Largest ship(3), Write a row(1-8) and a column (1-8) where you want the center of your ship to be");
+return board;
+
+int TryShipInput(int MaxValue, int MinValue){
+string Input = Console.ReadLine()?? "";
+int Value;
+
+while(!int.TryParse(Input, out Value)){
+    Console.WriteLine("Please Write a 2 digit number");
+    Input = Console.ReadLine()?? "";
+}
+Value = Convert.ToInt32(Input);
+
+while(Value < MinValue || Value > MaxValue){
+    Console.WriteLine("Please write a number between 9-89");
+    Input = Console.ReadLine()?? "";
+
+    while(!int.TryParse(Input, out Value)){
+    Console.WriteLine("Please Write a 2 digit number");
+    Input = Console.ReadLine()?? "";
+}
+
+}
+return Value;
+
+}
+void Shoot()
+{
+
+}
+void win()
+{
+
+}
--- BattleShip/ShipControl.cs
+++ BattleShip/ShipControl.cs
@@ -1,5 +1,12 @@
 class Ship{
+int shiptype1 = 1;
+int shiptype2 = 2;
+int shiptype3 = 3;
 
+int ThisShipType;
 
-    
+public Ship(int Shiptype){
+    ThisShipType = 1;
+}
+
 }
