e5847675fed3c870eeefd5f1542804430c095faf
Neo Rosenlund <Neo.Rosenlund@elev.ga.ntig.se>
2024-03-26 10:59:32 +0100

    fixed tryship to be cleaner, and made fill board function

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -5,13 +5,20 @@ string[,] board2 = new string[8, 8];
 string[,] ShotBoard1 = new string[8, 8];
 string[,] ShotBoard2 = new string[8, 8];
 
-PlaceShip(board1);
+
+// PlaceShip(board1);
 void Menu()
 {
-
 }
 void GameLoop()
 {
+Console.WriteLine("Player 1");
+board1 = PlaceShip(board1);
+Console.Clear();
+Console.WriteLine("Player 2");
+board2 = PlaceShip(board2);
+Console.Clear();
+
 
 }
 static string[,] PlaceShip(string[,] board)
@@ -27,33 +34,55 @@ Console.Clear();
         }
         Console.WriteLine();
     }
-Console.WriteLine("Place your Largest ship(3), Write a row(1-8) and a column (1-8) where you want the center of your ship to be");
-return board;
+    Console.WriteLine("Place your Largest ship(3), Write a row(1-8) and a column (1-8) where you want the center of your ship to be");
+    (int, int) BigShipP1 = TryShipInput(8, 8);
+    Console.WriteLine("Place your second largest ship(2), write a row(1-8) and a column (1-8) where the left part of the ship is going to be");
+    (int, int) MediumShipP1 = TryShipInput(8,8);
+    Console.WriteLine("Place your smallest ship(1), write a row(1-8) and a column (1-8) where you want the ship to be");
+    (int, int) SmalShipP1 = TryShipInput(8,8);
+    Console.Clear();
+    return board;
+}
 
-int TryShipInput(int MaxValue, int MinValue){
-string Input = Console.ReadLine()?? "";
+
+static (int, int) TryShipInput(int MaxValue, int MinValue){
 int Value;
-
-while(!int.TryParse(Input, out Value)){
-    Console.WriteLine("Please Write a 2 digit number");
-    Input = Console.ReadLine()?? "";
-}
-Value = Convert.ToInt32(Input);
-
-while(Value < MinValue || Value > MaxValue){
-    Console.WriteLine("Please write a number between 9-89");
+string Input;
+while(true){
+do{
+    Console.WriteLine($"Write a value between {MinValue} and {MaxValue}");
     Input = Console.ReadLine()?? "";
 
-    while(!int.TryParse(Input, out Value)){
-    Console.WriteLine("Please Write a 2 digit number");
-    Input = Console.ReadLine()?? "";
+}while(!int.TryParse(Input, out Value));
+
+if(Convert.ToInt32(Input) > MinValue && Convert.ToInt32(Input) < MaxValue){
+    if(Convert.ToString(Value).Split(' ')[0] == null && Convert.ToString(Value).Split(' ')[1] == null){
+        break;
+    }else{
+        Console.WriteLine("You already have a ship touching these positions!!!");
+    }
+}
 }
 
-}
-return Value;
+string Value1 = Convert.ToString(Input).Split(' ')[0];
+string Value2 = Convert.ToString(Input).Split(' ')[1];
+(int, int) Values = (Convert.ToInt32(Value1), Convert.ToInt32(Value2));
+return Values;
 
 }
-void Shoot()
+static string[,] FillBoard(string[,] board){
+    for(int i = 0; i < board.GetLength(0)-1; i++){
+        for(int j = 0; j < board.GetLength(1)-1; j++){
+            if(board[i,j] == null){
+                board[i,j] = "- ";
+            }
+        }
+    }
+return board;
+}
+
+
+void ShootPhase()
 {
 
 }
